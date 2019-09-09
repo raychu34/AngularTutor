19c02327cd06e8e874a35d1f3182916398cb8417
Ray Chu <rayallen1113.ee93@gmail.com>
2019-09-09 17:34:38 -0400

    obj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,22 +10,37 @@ export class AppComponent {
   isSubmitted = false;
   allVeggies = ['string bean', 'garlic', 'broccoli'];
   allProteins = ['chicken', 'beef', 'shrimp'];
+  allDishes = {
+    'string bean':{
+      'chicken': 'String Bean Chicken',
+      'beef': 'Beef with String Bean',
+    },
+    'garlic':{
+      'chicken': 'Garlic Chicken Fried Rice',
+      'beef': 'Garlic Beef fried noodles',
+      'shrimp': 'Garlic Shrimp'
+    },
+    'broccoli':{
+      'chicken': 'Broccoli Chicken',
+      'beef': 'Broccoli Beef',
+    }
+  }
   veggie: string;
   protein: string;
-  dishOrdered = 'test ' + this.veggie;
+  dishOrdered: string;
 
   orderDish() {
     this.isSubmitted = true;
-    if(this.veggie == undefined)
-    {
+    if(this.veggie === undefined) {
       this.dishOrdered = 'Please select a veggie';
-    }
-    else if(this.protein == undefined)
-    {
+    } else if (this.protein === undefined) {
       this.dishOrdered = 'Please select a protein';
-    }
-    else {
-      this.dishOrdered = this.veggie + ' ' + this.protein;
+    } else {
+      if (this.veggie in this.allDishes && this.protein in this.allDishes[this.veggie]) {
+        this.dishOrdered = this.allDishes[this.veggie][this.protein];
+      } else{
+        this.dishOrdered = '--';
+      }
     }
 
   }
